42fbe2f6d5582edf661d986b9ffe3be758385f54
Xenock20 <juliosaboredo@gmail.com>
2022-07-26 14:31:50 -0300

    Ejercicio 3 terminado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(suma(2,3,6));
+        Coche miCoche = new Coche();
+        miCoche.masPuertas();
+        System.out.println(miCoche.puertas);
+    }
+
+    public static int suma(int numUno, int numDos, int numTres) {
+        return numUno + numDos + numTres;
+    }
+}
+
+class Coche{
+    public int puertas;
+
+    public int masPuertas(){
+        return puertas++;
     }
 }
